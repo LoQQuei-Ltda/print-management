16695d20d31f7b4ac5de56404d0520ed369b518e
Eduardo Sirino <eduardo.sirino@loqquei.com.br>
2025-01-27 15:39:43 -0300

    correção coleta snmp

--- src/snmp/helpers/snmp.js
+++ src/snmp/helpers/snmp.js
@@ -7,9 +7,14 @@ const CONSTANTS = require('../../../helper/constants');
  * @param {string} port 
  * @param {string} community 
  * @param {string} oid 
- * @returns 
+ * @returns {Promise<{data: string}>}
  */
-const getPrintedPages = async (ip, port = CONSTANTS.SNMP.PORT, community = CONSTANTS.SNMP.COMMUNITY, oid = CONSTANTS.SNMP.OID_PAGES_TOTAL) => {
+const getPrintedPages = async (
+    ip,
+    port = CONSTANTS.SNMP.PORT,
+    community = CONSTANTS.SNMP.COMMUNITY,
+    oid = CONSTANTS.SNMP.OID_PAGES_TOTAL
+) => {
     return new Promise((resolve, reject) => {
         const session = snmp.createSession(ip, community, { port });
 
@@ -18,29 +23,29 @@ const getPrintedPages = async (ip, port = CONSTANTS.SNMP.PORT, community = CONST
 
             if (error) {
                 return reject({
-                    message: `Erro SNMP: ${error.toString()}`
+                    success: false,
+                    message: `Erro SNMP: ${error.message}`
                 });
             }
 
             for (const varbind of varbinds) {
                 if (snmp.isVarbindError(varbind)) {
                     return reject({
+                        success: false,
                         message: `Erro no varbind: ${snmp.varbindError(varbind)}`
                     });
                 }
             }
 
-            const data = varbinds[0].value.toString();
-
-            resolve({
-                data
-            });
+            const data = varbinds[0]?.value?.toString() || '';
+            resolve({ success: true, data });
         });
 
         session.on('error', (err) => {
             session.close();
             reject({
-                message: `Erro na sessão SNMP: ${err.toString()}`
+                success: false,
+                message: `Erro na sessão SNMP: ${err.message}`
             });
         });
     });
@@ -51,71 +56,82 @@ const getPrintedPages = async (ip, port = CONSTANTS.SNMP.PORT, community = CONST
  * @param {string} ip 
  * @param {string} port 
  * @param {string} community 
- * @returns 
+ * @returns {Promise<{[description: string]: string}>}
  */
-const getTonerLevels = async (ip, port = CONSTANTS.SNMP.PORT, community = CONSTANTS.SNMP.COMMUNITY) => {
-    const session = snmp.createSession(ip, community, { port });
-
+const getTonerLevels = async (
+    ip,
+    port = CONSTANTS.SNMP.PORT,
+    community = CONSTANTS.SNMP.COMMUNITY
+) => {
     return new Promise((resolve, reject) => {
+        const session = snmp.createSession(ip, community, { port });
         const tonerLevels = {};
 
-        const finalize = () => {
-            session.close();
-            resolve(tonerLevels);
-        };
+        session.walk(
+            CONSTANTS.SNMP.OID_DESCRIPTION,
+            (varbind) => {
+                if (snmp.isVarbindError(varbind)) {
+                    reject({
+                        success: false,
+                        message: `Erro no varbind de descrição: ${snmp.varbindError(varbind)}`
+                    });
+                    session.close();
+                    return false;
+                }
 
-        session.walk(CONSTANTS.SNMP.OID_DESCRIPTION, (varbind) => {
-            if (snmp.isVarbindError(varbind)) {
-                reject({
-                    success: false,
-                    message: `Erro no varbind de descrição: ${snmp.varbindError(varbind)}`
+                const oidParts = varbind.oid.split('.');
+                const index = oidParts[oidParts.length - 1];
+                const description = varbind.value.toString();
+                const oidLevel = `${CONSTANTS.SNMP.OID_LEVEL}.${index}`;
+
+                session.get([oidLevel], (error, varbindsLevel) => {
+                    if (error) {
+                        reject({
+                            success: false,
+                            message: `Erro ao consultar nível SNMP: ${error.message}`
+                        });
+                        session.close();
+                        return;
+                    }
+
+                    const varbindLevel = varbindsLevel[0];
+                    if (snmp.isVarbindError(varbindLevel)) {
+                        reject({
+                            success: false,
+                            message: `Erro no varbind de nível para OID ${varbindLevel.oid}: ${snmp.varbindError(varbindLevel)}`
+                        });
+                        session.close();
+                        return;
+                    }
+
+                    const level = varbindLevel.value?.toString() || '';
+                    tonerLevels[description] = level;
                 });
+            },
+            (error) => {
                 session.close();
-                return false;
-            }
-
-            const oidParts = varbind.oid.split('.');
-            const index = oidParts[oidParts.length - 1];
-            const description = varbind.value.toString();
-
-            const oidLevel = `${CONSTANTS.SNMP.OID_LEVEL}.${index}`;
-            session.get([oidLevel], (error, varbindsLevel) => {
                 if (error) {
                     reject({
                         success: false,
-                        message: `Erro ao consultar nível SNMP: ${error.toString()}`
+                        message: `Erro durante o walk SNMP: ${error.message}`
                     });
-                    session.close();
-                    return;
+                } else {
+                    resolve({ success: true, tonerLevels });
                 }
-
-                const varbindLevel = varbindsLevel[0];
-                if (snmp.isVarbindError(varbindLevel)) {
-                    reject({
-                        success: false,
-                        message: `Erro no varbind de nível para OID ${varbindLevel.oid}: ${snmp.varbindError(varbindLevel)}`
-                    });
-                    session.close();
-                    return;
-                }
-
-                const level = varbindLevel.value.toString();
-                tonerLevels[description] = level;
-            });
-        }, (error) => {
-            if (error) {
-                reject({
-                    success: false,
-                    message: `Erro durante o walk SNMP: ${error.toString()}`
-                });
-            } else {
-                finalize();
             }
+        );
+
+        session.on('error', (err) => {
+            session.close();
+            reject({
+                success: false,
+                message: `Erro na sessão SNMP: ${err.message}`
+            });
         });
     });
 };
 
 module.exports = {
-    getPrintedPages: getPrintedPages,
-    getTonerLevels: getTonerLevels
+    getPrintedPages,
+    getTonerLevels
 };
