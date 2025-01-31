766061b1e0b548eb38570bc2e3f6bf0811f9b181
Eduardo Sirino <eduardo.sirino@loqquei.com.br>
2025-01-31 14:57:33 -0300

    Alteração no git pull do script de atualização

--- update.sh
+++ update.sh
@@ -17,18 +17,32 @@ UPDATE_DIR="/opt/print-management/updates"
 EXECUTED_FILE="/opt/print-management/executed_updates.txt"
 
 cd /opt/print-management || exit 1
-CURRENT_COMMIT_HASH=$(git rev-parse HEAD)
+
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+if [ "$CURRENT_BRANCH" != "main" ]; then
+  echo "Repositório não está na branch main. Está na branch $CURRENT_BRANCH. Mudando para a branch main..."
+  run_command "git checkout main" "Erro ao mudar para a branch main."
+fi
+
+REMOTE_COMMIT_HASH=$(sudo git ls-remote origin HEAD | awk '{ print $1 }')
 
 if [ -f /opt/print-management/version.json ]; then
   SAVED_COMMIT_HASH=$(jq -r '.commit_hash' /opt/print-management/version.json)
+  
+  if [ -z "$SAVED_COMMIT_HASH" ]; then
+    echo "Erro: Hash do commit no version.json está vazio."
+    exit 1
+  fi
 else
   echo "Erro: Arquivo de informações de instalação não encontrado!"
   exit 1
 fi
 
-if [ "$CURRENT_COMMIT_HASH" != "$SAVED_COMMIT_HASH" ]; then
+if [ "$REMOTE_COMMIT_HASH" != "$SAVED_COMMIT_HASH" ]; then
   echo "Nova atualização encontrada. Iniciando atualização..."
 
+  run_command "git reset --hard" "Erro ao descartar alterações locais."
+  
   run_command "sudo git pull --no-verify origin main" "Erro ao atualizar repositório."
 
   NEW_COMMIT_HASH=$(git rev-parse HEAD)
@@ -47,11 +61,11 @@ if [ "$CURRENT_COMMIT_HASH" != "$SAVED_COMMIT_HASH" ]; then
 
   for i in $(seq -f "%02g" 1 99); do
     SCRIPT_FILE="$UPDATE_DIR/$i.sh"
-
+    
     if [ -f "$SCRIPT_FILE" ]; then
       if ! grep -q "$i" "$EXECUTED_FILE"; then
         echo "Executando atualização $i..."
-
+        
         run_command "sudo bash $SCRIPT_FILE" "Erro ao executar a atualização $i."
         echo "$i" | sudo tee -a "$EXECUTED_FILE" > /dev/null
         
