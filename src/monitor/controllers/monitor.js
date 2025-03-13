52ae7401685a3a2c0ed0d0367ce7b8040f1b534f
Eduardo Sirino <eduardo.sirino@loqquei.com.br>
2025-03-13 20:04:41 -0300

    Alteração do monitor

--- src/monitor/controllers/monitor.js
+++ src/monitor/controllers/monitor.js
@@ -76,154 +76,209 @@ const deleteOldFiles = async (dirPath) => {
     }
 }
 
-// Espera até que o arquivo não esteja sendo gravado e tenta renomeá-lo
-const waitForFileAndRename = async (filePath) => {
-    let attempts = 0;
-    let isLocked = true;
-    let lastModifiedTime = 0;
-
-    while (isLocked && attempts < 60) {
-        try {
-            const stats = await fs.promises.stat(filePath);
-            const currentModifiedTime = stats.mtime.getTime();
-
-            if (currentModifiedTime === lastModifiedTime) {
-                // Tenta renomear o arquivo
-                const tempPath = filePath + '.tmp';
-                await fs.promises.rename(filePath, tempPath);
-                await fs.promises.rename(tempPath, filePath);
-                isLocked = false;
-            } else {
-                lastModifiedTime = currentModifiedTime;
-                console.log(`Esperando o arquivo ${filePath} terminar de ser gravado...`);
-                await new Promise(resolve => setTimeout(resolve, 1000));
-            }
-        } catch (error) {
-            console.log(`Arquivo ${filePath} ainda está em uso...`);
-            await new Promise(resolve => setTimeout(resolve, 1000));
+const isFileAvailable = async (filePath, previousSize = -1, attempts = 0) => {
+    try {
+        if (!fs.existsSync(filePath)) {
+            return false;
         }
-
-        attempts++;
+        
+        const fileHandle = await fs.promises.open(filePath, 'r+');
+        await fileHandle.close();
+        
+        const stats = await fs.promises.stat(filePath);
+        const currentSize = stats.size;
+        
+        if (previousSize !== -1 && previousSize !== currentSize) {
+            await new Promise(resolve => setTimeout(resolve, 500));
+            return isFileAvailable(filePath, currentSize, attempts + 1);
+        }
+        
+        if (previousSize === -1 || currentSize === 0) {
+            await new Promise(resolve => setTimeout(resolve, 500));
+            return isFileAvailable(filePath, currentSize, attempts + 1);
+        }
+        
+        if (attempts > 0 && previousSize === currentSize) {
+            return true;
+        }
+        
+        await new Promise(resolve => setTimeout(resolve, 500));
+        return isFileAvailable(filePath, currentSize, attempts + 1);
+    } catch (error) {
+        if (attempts >= 120) {
+            Log.error({
+                entity: CONSTANTS.LOG.MODULE.MONITOR,
+                operation: 'isFileAvailable',
+                errorMessage: `Arquivo ${filePath} indisponível após 30 segundos de tentativas`,
+                errorStack: error.stack
+            });
+            return false;
+        }
+        
+        await new Promise(resolve => setTimeout(resolve, 500));
+        return isFileAvailable(filePath, previousSize, attempts + 1);
     }
+};
 
-    if (isLocked) {
+const safelyCopyFile = async (sourcePath, destPath) => {
+    try {
+        const fileContent = await fs.promises.readFile(sourcePath);
+        
+        await fs.promises.writeFile(destPath, fileContent);
+        
+        if (fs.existsSync(destPath)) {
+            try {
+                await fs.promises.unlink(sourcePath);
+            } catch (deleteError) {
+                Log.error({
+                    entity: CONSTANTS.LOG.MODULE.MONITOR,
+                    operation: 'SafelyCopyFile',
+                    errorMessage: `Não foi possível excluir o arquivo original ${sourcePath}`,
+                    errorStack: deleteError.stack
+                });
+            }
+            return true;
+        }
+        return false;
+    } catch (error) {
         Log.error({
             entity: CONSTANTS.LOG.MODULE.MONITOR,
-            operation: 'WaitForFileAndRename',
-            errorMessage: `O arquivo ${filePath} ainda está sendo gravado após múltiplas tentativas.`,
-            errorStack: ''
+            operation: 'SafelyCopyFile',
+            errorMessage: `Erro ao copiar arquivo de ${sourcePath} para ${destPath}`,
+            errorStack: error.stack
+        });
+        return false;
+    }
+};
+
+const processNewFile = async (filePath) => {
+    try {
+        const ext = path.extname(filePath);
+        const fileExtension = ext.toLowerCase();
+
+        if (fileExtension !== '.pdf') {
+            await deleteFile(filePath);
+            return;
+        }
+
+        if (path.dirname(filePath) === CONSTANTS.SAMBA.BASE_PATH_FILES) {
+            await deleteFile(filePath);
+            return;
+        }
+
+        const fileNameSave = path.basename(filePath);
+        const fileName = fileNameSave.replace(ext, '');
+
+        const fileAvailable = await isFileAvailable(filePath);
+        if (!fileAvailable) {
+            return;
+        }
+
+        const result = await FilesModel.getById(fileName);
+        if (result && result.id) {
+            return;
+        }
+
+        const id = uuid();
+
+        const relativePath = path.relative(CONSTANTS.SAMBA.BASE_PATH_FILES, path.dirname(filePath));
+        const parts = relativePath.split(path.sep);
+        if (!parts || parts.length === 0) {
+            Log.error({
+                entity: CONSTANTS.LOG.MODULE.MONITOR,
+                operation: 'Extract User',
+                errorMessage: `Não foi possível extrair o usuário a partir do caminho: ${filePath}`,
+                errorStack: ''
+            });
+            return;
+        }
+
+        const userIdDashless = parts[0];
+
+        const userResult = await User.getByUsername(userIdDashless);
+        let user;
+        if (Array.isArray(userResult)) {
+            user = userResult[0];
+        } else {
+            user = userResult;
+        }
+
+        if (!user || !user.id) {
+            Log.error({
+                entity: CONSTANTS.LOG.MODULE.MONITOR,
+                operation: 'Get User',
+                errorMessage: `Usuário não encontrado para: ${userIdDashless}`,
+                errorStack: ''
+            });
+            return;
+        }
+
+        const userId = user.id;
+
+        const pages = await getPages(filePath);
+        if (pages === 'Error') {
+            return;
+        }
+
+        const newFilePath = path.join(path.dirname(filePath), id + ext);
+
+        const data = [id, userId, null, fileNameSave, pages, newFilePath, new Date(), null, false, false];
+        await FilesModel.insert(data);
+
+        const copySuccess = await safelyCopyFile(filePath, newFilePath);
+        if (!copySuccess) {
+            await FilesModel.delete(id);
+            Log.error({
+                entity: CONSTANTS.LOG.MODULE.MONITOR,
+                operation: 'Process New File',
+                errorMessage: `Não foi possível copiar o arquivo ${filePath} para ${newFilePath}`,
+                errorStack: ''
+            });
+        }
+    } catch (error) {
+        Log.error({
+            entity: CONSTANTS.LOG.MODULE.MONITOR,
+            operation: 'Process New File',
+            errorMessage: error.message,
+            errorStack: error.stack
         });
     }
 };
 
+const pendingFiles = new Map();
+
 module.exports = {
     monitorStart: async () => {
-        const lastFile = new Set();
-
         if (!fs.existsSync(CONSTANTS.SAMBA.BASE_PATH_FILES)) {
             await fs.mkdirSync(CONSTANTS.SAMBA.BASE_PATH_FILES);
         }
 
         const watcher = chokidar.watch(CONSTANTS.SAMBA.BASE_PATH_FILES, {
+            // eslint-disable-next-line no-useless-escape
             ignored: /(^|[\/\\])\../,
             persistent: true,
             ignoreInitial: false,
             depth: 99999,
-            followSymlinks: false
+            followSymlinks: false,
+            awaitWriteFinish: {
+                stabilityThreshold: 2000,
+                pollInterval: 500
+            }
         });
 
-        watcher.on('add', async (filePath) => {
-            try {
-                const ext = path.extname(filePath);
-                const fileExtension = ext.toLowerCase();
-
-                if (fileExtension !== '.pdf') {
-                    await deleteFile(filePath);
-                    return;
-                }
-
-                if (path.dirname(filePath) === CONSTANTS.SAMBA.BASE_PATH_FILES) {
-                    await deleteFile(filePath);
-                    return;
-                }
-
-                const fileNameSave = path.basename(filePath);
-                const fileName = fileNameSave.replace(ext, '');
-
-                if (lastFile.has(fileName)) {
-                    return;
-                }
-
-                const result = await FilesModel.getById(fileName);
-                if (result && result.id) {
-                    return;
-                }
-
-                const id = uuid();
-                lastFile.add(id);
-
-                const relativePath = path.relative(CONSTANTS.SAMBA.BASE_PATH_FILES, path.dirname(filePath));
-                const parts = relativePath.split(path.sep);
-                if (!parts || parts.length === 0) {
-                    Log.error({
-                        entity: CONSTANTS.LOG.MODULE.MONITOR,
-                        operation: 'Extract User',
-                        errorMessage: `Não foi possível extrair o usuário a partir do caminho: ${filePath}`,
-                        errorStack: ''
-                    });
-                    return;
-                }
-
-                const userIdDashless = parts[0];
-
-                const userResult = await User.getByUsername(userIdDashless);
-                let user;
-                if (Array.isArray(userResult)) {
-                    user = userResult[0];
-                } else {
-                    user = userResult;
-                }
-
-                if (!user || !user.id) {
-                    Log.error({
-                        entity: CONSTANTS.LOG.MODULE.MONITOR,
-                        operation: 'Get User',
-                        errorMessage: `Usuário não encontrado para: ${userIdDashless}`,
-                        errorStack: ''
-                    });
-                    return;
-                }
-
-                const userId = user.id;
-
-                // Espera o arquivo ser gravado e não estar bloqueado
-                await waitForFileAndRename(filePath);
-
-                const pages = await getPages(filePath);
-
-                if (pages === 'Error') {
-                    return;
-                }
-
-                const newFilePath = path.join(path.dirname(filePath), id + ext);
-
-                const data = [id, userId, null, fileNameSave, pages, newFilePath, new Date(), null, false, false];
-
-                await FilesModel.insert(data);
-
-                try {
-                    await fs.promises.rename(filePath, newFilePath);
-                } catch (error) {
-                    Log.error({
-                        entity: CONSTANTS.LOG.MODULE.MONITOR,
-                        operation: 'Add',
-                        errorMessage: error.message,
-                        errorStack: error.stack
-                    });
-                }
-            } catch (error) {
-                console.error("Erro ao processar o arquivo:", filePath, error);
+        watcher.on('add', (filePath) => {
+            const fileId = filePath.toLowerCase();
+            
+            if (pendingFiles.has(fileId)) {
+                return;
             }
+            
+            pendingFiles.set(fileId, true);
+            
+            setTimeout(async () => {
+                await processNewFile(filePath);
+                pendingFiles.delete(fileId);
+            }, 1000);
         });
 
         watcher.on('change', async (filePath) => {
@@ -255,6 +310,8 @@ module.exports = {
         });
 
         watcher.on('error', async (error) => {
+            console.error("Erro ao monitorar o diretório:", error);
+
             Log.error({
                 entity: CONSTANTS.LOG.MODULE.MONITOR,
                 operation: 'Monitor',
